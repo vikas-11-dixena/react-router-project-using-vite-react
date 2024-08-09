a8659db5cc1837806ab8d2b34f0257ea0ee4faf0
vikas-11-dixena <vikasdixena79@gmail.com>
2024-08-09 14:11:39 +0530

    home and about page path setup and also link changes into navlink differeces

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import { Route, Routes } from 'react-router'
-import Home from './pages/home/Home'
-import About from './pages/about/About'
+import React from "react";
+import { Route, Routes } from "react-router";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
 
 function App() {
   return (
     <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Find Jobs ?</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
       <main>
         <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='about' element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
         </Routes>
       </main>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
